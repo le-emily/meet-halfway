6487cee5fd58ddea57d97ba31c081629a8e2cdce
vagrant <vagrant@vagrant.vm>
2017-05-17 23:58:15 +0000

    refactored codeAddress function for midpoint

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,3 @@
-var coordinates = [];
 var geocoder;
 var map;
 
@@ -18,7 +17,8 @@ function initialize() {
   function onSubmit(evt) {
     evt.preventDefault();
     calculateAndDisplayRoute(directionsService, directionsDisplay);
-    codeAddress_a();
+    codeAddress();
+    calculateMidpoint();
   }
 
   document.getElementById("search").addEventListener("click", onSubmit);
@@ -40,66 +40,36 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   });
 }
 
-function codeAddress_a() {
-  var address = document.getElementById('location_a').value;
-  var coord_a = [];
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == 'OK') {
+function codeAddress() {
+  var location = document.getElementsByName('location');
+  var coord = [];
+  for(var x=0; x < location.length; x++) {
+    var address = location[x].value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
 
-      coord_a[0] = results[0].geometry.location.lat();
-      coord_a[1] = results[0].geometry.location.lng();
+        coord.push(results[0].geometry.location.lat());
+        coord.push(results[0].geometry.location.lng());
 
-      map.setCenter(results[0].geometry.location);
-      // var marker = new google.maps.Marker({
-      //     map: map,
-      //     position: results[0].geometry.location
-      // });
-      // console.log(location_a);
-      // console.log("Success function");
-      coordinates.push.apply(coordinates, coord_a);
-      codeAddress_b();
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-    return coord_a;
-  });
-  console.log("The end");
-  
+        map.setCenter(results[0].geometry.location);
+        // coordinates.push.apply(coordinates, coord);
+        if (coord.length == 4) {
+          // console.log("coord variable has:");
+          // console.log(coord);
+          return calculateMidpoint(coord);
+        }
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+      
+    });
+    console.log("The end");
+  }
 }
 
-function codeAddress_b() {
-  // marker will not show without preventDefault
-  // evt.preventDefault();
-  var address = document.getElementById('location_b').value;
-  var coord_b = [];
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == 'OK') {
-
-      coord_b[0] = results[0].geometry.location.lat();
-      coord_b[1] = results[0].geometry.location.lng();
-
-      map.setCenter(results[0].geometry.location);
-      // var marker = new google.maps.Marker({
-      //     map: map,
-      //     position: results[0].geometry.location
-      // });
-      coordinates.push.apply(coordinates, coord_b);
-      calculateMidpoint();
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-    return coord_b;
-  });
-  
-}
-
-function calculateMidpoint() {
-  console.log(coordinates);
-  var _lat = (coordinates[0] + coordinates[2])/2.0;
-  var _lng = (coordinates[1] + coordinates[3])/2.0;
-
-  console.log(_lat);
-  console.log(_lng);
+function calculateMidpoint(evt) {
+  var _lat = (evt[0] + evt[2])/2.0;
+  var _lng = (evt[1] + evt[3])/2.0;
 
   var coords = {"lat": _lat, "lng": _lng};
 
